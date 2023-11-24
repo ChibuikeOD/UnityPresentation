03104899044c6df115d83c809457f4a8d1153f04
raw5851 <raw5851@psu.edu>
2023-11-24 13:46:06 -0500

    Text to speech initial implementation

--- Voice Automation/Assets/Speech.cs
+++ Voice Automation/Assets/Speech.cs
@@ -10,6 +10,11 @@ using UnityEngine.Windows.Speech;
 using TMPro;
 using HuggingFace.API;
 
+using System.Collections.Concurrent;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using SpeechGenerationSystem;
+
 
 namespace Speech
 {
@@ -35,6 +40,13 @@ namespace Speech
         public string fileName;
         public TextMeshProUGUI result;
 
+        //TTS Variables
+        public string espeakDirPath = "SGS/espeak-ng-data/";
+        public string voiceModelFilePath = "SGS/voices/en-us-kathleen-low.onnx";
+        public string voiceConfigFilePath = "SGS/voices/en-us-kathleen-low.onnx.json";
+        public ulong voiceId = 0;
+
+        public AudioSource audioPlayer = null;
 
         void Start()
         {
@@ -55,6 +67,31 @@ namespace Speech
             keywordRec = new KeywordRecognizer(keywords.Keys.ToArray()); //initialize speech rec
 
             keywordRec.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized; //Register
+
+            //tts
+            var espeakDataFPath = $"{Application.streamingAssetsPath}/{espeakDirPath}";
+            var voiceModelFPath = $"{Application.streamingAssetsPath}/{voiceModelFilePath}";
+            var voiceConfigFPath = $"{Application.streamingAssetsPath}/{voiceConfigFilePath}";
+            var initResult = _sgsApi.Init(espeakDataFPath, voiceModelFPath, voiceConfigFPath, voiceId);
+            switch (initResult)
+            {
+                case InitResult.Success:
+                    _inited = true;
+                    _processingTask = Task.Run(ProcessingAsync);
+                    break;
+                case InitResult.ESpeakNotFound:
+                    Debug.LogErrorFormat("USgs. Directory {0} not found!", espeakDataFPath);
+                    break;
+                case InitResult.VoiceModelNotFound:
+                    Debug.LogErrorFormat("USgs. File {0} not found!", voiceModelFPath);
+                    break;
+                case InitResult.VoiceConfigNotFound:
+                    Debug.LogErrorFormat("USgs. File {0} not found!", voiceConfigFPath);
+                    break;
+                case InitResult.VoiceIsNull:
+                    Debug.LogErrorFormat("USgs. Voice is null!");
+                    break;
+            }
 
             // Start the recognizer
             keywordRec.Start();
@@ -105,6 +142,112 @@ namespace Speech
                 Save();
             }
         }
+
+        //tts--------------------------------------------------------------------
+        private async Task ProcessingAsync()
+        {
+            const int DELAY_MS = 100;
+            while (_inited)
+            {
+                if (_textQueue.TryDequeue(out string text))
+                {
+                    if (_sgsApi.GenerateSpeech(text))
+                    {
+                        _pcmDataQueue.Enqueue(_sgsApi.PcmData);
+                    }
+                    else
+                    {
+                        Debug.LogErrorFormat("USgs. Failed to generate audio for the text {0}", text);
+                    }
+                }
+                else
+                {
+                    await Task.Delay(DELAY_MS);
+                }
+            }
+        }
+
+        public void ReceiveTextToSpeech(string text)
+        {
+            const int LETTER_LIMIT = 400;
+            const string WORDS_DELIMITER = " ";
+            var fragmentsQueue = new Queue<string>(_sentenceSplitPattern.Split(text));
+            while (fragmentsQueue.Count > 0)
+            {
+                var fragment = fragmentsQueue.Dequeue();
+                if (fragmentsQueue.Count > 0)
+                {
+                    fragment += fragmentsQueue.Dequeue();
+                }
+                if (fragment.Length > LETTER_LIMIT)
+                {
+                    var wordsQueue = new Queue<string>(fragment.Split(WORDS_DELIMITER));
+                    var sb = new StringBuilder();
+                    while (wordsQueue.Count > 0)
+                    {
+                        if (sb.Length < LETTER_LIMIT)
+                        {
+                            var word = wordsQueue.Dequeue() + WORDS_DELIMITER;
+                            sb.Append(word);
+                        }
+                        else
+                        {
+                            _textQueue.Enqueue(sb.ToString());
+                            sb.Clear();
+                        }
+                    }
+                    if (sb.Length > 0)
+                    {
+                        _textQueue.Enqueue(sb.ToString());
+                    }
+                }
+                else
+                {
+                    _textQueue.Enqueue(fragment);
+                }
+            }
+        }
+
+        private SgsApi _sgsApi = new SgsApi();
+        private bool _inited = false;
+        private readonly ConcurrentQueue<string> _textQueue = new ConcurrentQueue<string>();
+        private readonly ConcurrentQueue<PcmData> _pcmDataQueue = new ConcurrentQueue<PcmData>();
+        private Regex _sentenceSplitPattern = new Regex(@"(\.|\!|\?|\n)");
+        private Task _processingTask = null;
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            if (!audioPlayer.isPlaying)
+            {
+                if (_pcmDataQueue.TryDequeue(out PcmData pcmData))
+                {
+                    var audioClip = AudioClip.Create("default", pcmData.Length, pcmData.Channels, pcmData.SampleRate, false);
+                    audioClip.SetData(pcmData.Value, 0);
+                    audioPlayer.clip = audioClip;
+                    audioPlayer.Play();
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _inited = false;
+            if (audioPlayer.isPlaying)
+            {
+                audioPlayer.Stop();
+            }
+            if (_processingTask != null)
+            {
+                _processingTask.Wait();
+                _processingTask = null;
+            }
+            if (_sgsApi != null)
+            {
+                _sgsApi.Dispose();
+                _sgsApi = null;
+            }
+        }
+        //end tts----------------------------------------------------------------
 
         private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
         {
@@ -162,6 +305,7 @@ namespace Speech
             {
                 result.color = Color.white;
                 result.text = response;
+                ReceiveTextToSpeech(response);
             }, error =>
             {
                 result.color = Color.red;
